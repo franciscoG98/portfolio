2280e849acdbda6582b54f589aa5ab397df3ca56
pacncho <franciscoG98>
2021-02-11 17:20:47 -0300

    light/dark theme working c:

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import {useState} from 'react';
 import './App.css';
-import Welcome from './components/Welcome';
 import Theme from './components/Theme';
 
 function App() {
 
-  const [theme, setTheme] = useState(false);
-
-  // setTheme(true);
   
 
   return (
     <div className="App">
         <Theme />
-        <Welcome theme={theme}/>
     </div>
   );
 }
--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Theme.css';
 
 export default function Theme() {
+
+    const [theme, setTheme] = useState(true)
+
+    const mode = theme ? 'light' : 'dark';
+
     return (
-        <label>
-            <input type="checkbox" />
-            <span className="check"></span>
-        </label>
+        <div>
+            <label>
+                <input type="checkbox" />
+                <span className="check" id={mode} onClick={() => setTheme(!theme)}></span>
+            </label>
+
+            {/* {console.log('theme:\n', theme, '\n mode \n', mode)} */}
+
+            <div className={mode}>
+                <h1>Hello!</h1>
+                <span>I'm Francisco, I like coding :)</span>
+            </div>
+        </div>
     )
 }
 
