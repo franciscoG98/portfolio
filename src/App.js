a1d3d283be74e7b29fbd2148f22c5db2b2db8503
pacncho <franciscoG98>
2021-02-24 11:18:20 -0300

    fixed navbar redireccion and reorganized folder

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import NavBar from './components/Navbar';
 import Home from './components/pages/home';
-import About from './components/pages/About';
-import Skills from './components/pages/Skills';
-import Work from './components/pages/Work';
-import Contact from './components/pages/Contact';
+import About from './components/pages/about/About';
+import Skills from './components/pages/skills/Skills';
+import Work from './components/pages/work/Work';
+import Contact from './components/pages/contact/Contact';
 
 
 function App() {
 
   const [theme, setTheme] = useState(true);
 
-  theme ? console.log('theme: ', theme, '\n light') : console.log('theme: ', theme, '\n dark')
+  // theme ? console.log('theme: ', theme, '\n light') : console.log('theme: ', theme, '\n dark')
 
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <NavBar theme={theme} setTheme={setTheme} />
       <Switch>
-        <Route exact path='/portfolio' component={Home} theme={theme} />
-        <Route exact path='/portfolio/about' component={About} theme={theme} />
-        <Route exact path='/portfolio/skills' component={Skills} theme={theme} />
-        <Route exact path='/portfolio/work' component={Work} theme={theme} />
-        <Route exact path='/portfolio/contact' component={Contact} theme={theme} />
+        <Route exact path='/' component={Home} theme={theme} />
+        <Route exact path='/about' component={About} theme={theme} />
+        <Route exact path='/skills' component={Skills} theme={theme} />
+        <Route exact path='/work' component={Work} theme={theme} />
+        <Route exact path='/contact' component={Contact} theme={theme} />
       </Switch>
     </Router>
   );
