6b7e4fc8c8e56a5398346357e2eb5faf6e4dd847
pacncho <franciscoG98>
2021-02-26 21:08:32 -0300

    skills content

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import './skillsStyles.css'
 
-function Skills() {
+function Skills(props) {
+
+    let fondo = props.theme ? 'dei' : 'nait';
+    let skills = fondo + ' mainCont';
+    
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
+        <div className={skills}>
             <h1>Skills</h1>
+
+            <div className='tech-cont'>
+                <h3>Front-end</h3>
+                <ul>
+                    <li>Javascript ES6</li>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>React-native</li>
+                    <li>CSS3</li>
+                    <li>Bootstrap</li>
+                    <li>Material UI</li>
+                    <li>Expo-cli</li>
+                </ul>
+            </div>
+            <div className='tech-cont'>
+                <h3>Back-end</h3>
+                <ul>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>PostgreSQL</li>
+                    <li>GraphQL</li>
+                    <li>SQLite</li>
+                    <li>MongoDB</li>
+                </ul>
+            </div>
+            <div className='tech-cont'>
+                <h3>other-ends</h3>
+                <ul>
+                    <li>Git(github)</li>
+                    <li>Linux Systems</li>
+                    <li>SCRUM Methodology</li>
+                </ul>
+            </div>
+
         </div>
     )
 }
