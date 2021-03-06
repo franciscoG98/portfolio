86a72a15a8632d4ae4968dd1c4d199fcc6bdd2ee
pancho <franciscoG98>
2021-03-06 18:46:35 -0300

    skills positioning

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -26,47 +26,52 @@ function Skills(props) {
     console.log('theme en skills', props.theme);
 
 
-    let fondo = props.theme ? 'dei' : 'nait';
-    let skills = fondo + ' mainCont';
+    // let fondo = props.theme ? 'dei' : 'nait';
+    // let skills = fondo + ' mainCont';
     
     return (
-        <div className={skills}>
+        // <div className={skills}>
+
+        <div className="skillsCont">
             <h1>Skills</h1>
 
-            <div className='tech-cont'>
-                <h3>Front-end</h3>
-                <ul>
-                    <li><DiJavascript1 size={40} />Javascript ES6</li>
-                    <li><DiReact size={40} />React</li>
-                    <li><DiReact size={40} />React-native</li>
-                    <li><DiCss3 size={40} />CSS3</li>
-                    <li><DiBootstrap size={40} />Bootstrap</li>
-                    <li><SiMaterialUi size={40} />Material UI</li>
-                    <li><SiRedux size={40} />Redux</li>
-                    <li><SiExpo size={40} />Expo-cli</li>
-                </ul>
+            <div className="tech-cont">
+                <div className='tech-cont1'>
+                    <h3>Front-end</h3>
+                    <ul>
+                        <li><DiJavascript1 size={40} />Javascript ES6</li>
+                        <li><DiReact size={40} />React</li>
+                        <li><DiReact size={40} />React-native</li>
+                        <li><DiCss3 size={40} />CSS3</li>
+                        <li><DiBootstrap size={40} />Bootstrap</li>
+                        <li><SiMaterialUi size={40} />Material UI</li>
+                        <li><SiRedux size={40} />Redux</li>
+                        <li><SiExpo size={40} />Expo-cli</li>
+                    </ul>
+                </div>
+                <div className='tech-cont2'>
+                    <h3>Back-end</h3>
+                    <ul>
+                        <li><SiNodeDotJs size={40} />Node</li>
+                        <li>Express</li>
+                        <li><DiSqllite size={40} />SQLite</li>
+                        <li><SiPostgresql size={40} />PostgreSQL</li>
+                        <li><SiGraphql size={40} />GraphQL</li>
+                        <li><SiMongodb size={40} />MongoDB</li>
+                    </ul>
+                </div>
+                <div className='tech-cont3'>
+                    <h3>other-ends</h3>
+                    <ul>
+                        <li><DiGit size={40} />Git</li>
+                        <li><DiLinux size={40} />Linux</li>
+                        <li><DiScrum size={40} />SCRUM Methodology</li>
+                    </ul>
+                </div>
             </div>
-            <div className='tech-cont'>
-                <h3>Back-end</h3>
-                <ul>
-                    <li><SiNodeDotJs size={40} />Node</li>
-                    <li>Express</li>
-                    <li><DiSqllite size={40} />SQLite</li>
-                    <li><SiPostgresql size={40} />PostgreSQL</li>
-                    <li><SiGraphql size={40} />GraphQL</li>
-                    <li><SiMongodb size={40} />MongoDB</li>
-                </ul>
-            </div>
-            <div className='tech-cont'>
-                <h3>other-ends</h3>
-                <ul>
-                    <li><DiGit size={40} />Git</li>
-                    <li><DiLinux size={40} />Linux</li>
-                    <li><DiScrum size={40} />SCRUM Methodology</li>
-                </ul>
-            </div>
-
         </div>
+
+        // </div>
     )
 }
 
