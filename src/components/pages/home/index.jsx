216be1856731b276c434e20a7e1ea38a13beb010
pancho <franciscoG98>
2021-03-07 02:12:14 -0300

    nothing really important

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -9,18 +9,6 @@ function Home(props) {
     // const str = "I'm a Full Stack Developer front-end oriented from Argentina. I love design and build responsive websites. My goal is to make web pages or mobile apps as prettier as they can be, with the best UX (user experience) possible without forgetting the perfomance. The internet is to make our lives easier :)"
 
 
-    // var content = "I'm a Full Stack Developer front-end oriented from Argentina. I love design and build responsive websites. My goal is to make web pages or mobile apps as prettier as they can be, with the best UX (user experience) possible without forgetting the perfomance. The internet is to make our lives easier :)";
-
-    // var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
-    // $(ele).hide().appendTo('p').each(function (i) {
-    //     $(this).delay(100 * i).css({
-    //         display: 'inline',
-    //         opacity: 0
-    //     }).animate({
-    //         opacity: 1
-    //     }, 100);
-    // });
-
     return (
         // <div className={theme}>
             <div className='containerH'>
@@ -31,8 +19,6 @@ function Home(props) {
                     <span>(user experience) possible without forgetting the perfomance. The </span>
                     <span>internet is to make our lives easier :)</span>                    
                 </p> */}
-                {/* <p></p> */}
-
                 <h1></h1>
             </div>
         // </div>
