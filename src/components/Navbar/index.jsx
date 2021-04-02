d6801399ffc249de428e2acff47103abd4a8aed3
Francisco <franciscomanuel.gonzalez3@gmail.com>
2021-04-02 15:44:10 -0300

    responsive navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -21,11 +21,11 @@ function Nabvar(props) {
             <Theme theme={props.theme} setTheme={props.setTheme} />
             <div >
                 <nav id='nav' className={props.theme} >
-                    <NavLink activeClassName= 'active' className='link' exact to="/"><AiOutlineHome/>Home</NavLink>
-                    <NavLink activeClassName= 'active' className='link' to="/about/"><AiOutlineBook />About</NavLink>
-                    <NavLink activeClassName= 'active' className='link' to="/work/"><AiOutlineLaptop />Work</NavLink>
-                    <NavLink activeClassName= 'active' className='link' to="/skills/"><AiOutlineCode />Skills</NavLink>
-                    <NavLink activeClassName= 'active' className='link' to="/contact/"><AiOutlineMail />Contact</NavLink>
+                    <NavLink activeClassName= 'active' className='link' exact to="/"><AiOutlineHome/><span>Home</span></NavLink>
+                    <NavLink activeClassName= 'active' className='link' to="/about/"><AiOutlineBook /><span>About</span></NavLink>
+                    <NavLink activeClassName= 'active' className='link' to="/work/"><AiOutlineLaptop /><span>Work</span></NavLink>
+                    <NavLink activeClassName= 'active' className='link' to="/skills/"><AiOutlineCode /><span>Skills</span></NavLink>
+                    <NavLink activeClassName= 'active' className='link' to="/contact/"><AiOutlineMail /><span>Contact</span></NavLink>
                 </nav>
             </div>
         </Container>
