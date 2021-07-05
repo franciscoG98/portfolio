e8b03d6face18162ccf37ed84900da2c3bbe207d
Francisco <franciscomanuel.gonzalez3@gmail.com>
2021-07-05 02:15:22 -0300

    work section, edit card styles, proyect component

--- README.md
+++ README.md
@@ -13,18 +13,22 @@ You can see it [here](https://franciscog98.github.io/portfolio/)
 <h3>SKILLS</h3>
 <img src='https://github.com/franciscoG98/portfolio/blob/master/public/img/skillsPic.png' alt='skills preview'/>
 
-<h3>CONTACT</h3>
-<img src='https://github.com/franciscoG98/portfolio/blob/master/public/img/contactPic.png' alt='contact preview'/>
+<h3>WORK</h3>
+<img src='https://github.com/franciscoG98/portfolio/blob/master/public/img/workPic.png' alt='work preview'/>
 
 
 # Tutorials/resources: 
 * [gh-pages deploy and domain](https://www.youtube.com/watch?v=SKXkC4SqtRk).
 * [navigation](https://youtu.be/VzWBLj_CfpE).
 * [theme](https://www.youtube.com/watch?v=_f036OpnuWo). This feature was taken off for simplicity, If you wanna see how it works it is in remake branch
-* [zoom image with css](http://www.corelangs.com/css/box/zoom.html).
 
-# What I've use
-I've done this with create-react-app, react-router-dom, react-icons and css
+# Stack:
+* [Create-react-app](https://reactjs.org/docs/create-a-new-react-app.html)
+* [ReactJS](https://reactjs.org/)
+* [React Bootstrap](https://react-bootstrap.github.io/)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [React Icons](https://react-icons.github.io/react-icons/)
+
 
 # Code
 To code you will need to have installed [node](https://nodejs.org/es/)and [npm](https://www.npmjs.com/) (I have node v15.11 and npm v7.6 but I think you can try with lts versions and it'll work)
@@ -37,5 +41,8 @@ npm start
 You can also fork this repository and publish your own portfolio
 
 # Build
-npm run build && npm run deploy
+This site is doplyed on Github-Pages.
 
+```
+npm run build && npm run deploy
+```
--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -1,62 +1,73 @@
-// import React from 'react';
+import React from 'react';
 import './workStyles.css';
-import catGif from './giphy.gif'
 import { Col, Row } from 'react-bootstrap'
+import Proyect from '../../components/Proyect'
 
-function Work() {
+
+const Work = () => {
+
+    const BBCimg = 'https://raw.githubusercontent.com/franciscoG98/BreakingBadApp2/main/src/img/home.png';
+    const BBClink = 'https://franciscog98.github.io/BreakingBadApp2/';
+    const BBCrepo = 'https://github.com/franciscoG98/BreakingBadApp2';
+
+    const Moviesimg = 'https://raw.githubusercontent.com/franciscoG98/movies-app/main/media/homeScreen.png';
+    const Movieslink = 'https://movies-app-fg.vercel.app/';
+    const Moviesrepo = 'https://github.com/franciscoG98/movies-app';
+    
+    
+
     return (
         
         <div className='workContainer'>
-            <Col>
-                <Row>
-                    <h3>This section is under construction</h3>
-                </Row>
-                <Row>
-                    <Col></Col>
-                    <Col>
-                        <img className='construction' src={catGif} alt="crazy cat coding"/>
-                    </Col>
-                    <Col></Col>
-                </Row>
-            </Col>
+
+            <Row className='cardRow'>
+                <Col xs={10} sm={8} md={5} lg={5}>
+                    <Proyect
+                        title={'Breaking Bad'}
+                        image={BBCimg}
+                        imgAlt={'breaking bad cast site preview'}
+                        repo={BBCrepo}
+                        siteLink={BBClink}
+                        description={'Static web page where you cand find you favorite characters from Breaking Bad. Stack: react, css, gh-pages'}
+                    />
+                </Col>
+
+                <Col xs={10} sm={8} md={5} lg={5}>
+                    <Proyect
+                        title={'Movies App'}
+                        image={Moviesimg}
+                        imgAlt={'movies app site preview'}
+                        repo={Moviesrepo}
+                        siteLink={Movieslink}
+                        description={'Static web page where you cand find you favorite movies and series. Stack: react, css, OMDB Api, vercel'}
+                    />
+                </Col>
+
+                {/* <Col xs={10} sm={8} md={5} lg={5}>
+                    <Proyect
+                        title={'Breaking Bad Cast'}
+                        image={'https://github.com/franciscoG98/BreakingBadApp2/blob/main/src/img/home.png'}
+                        imgAlt={'breaking bad cast site preview'}
+                        repo={'https://github.com/franciscoG98/BreakingBadApp2'}
+                        // siteLink={}
+                        description={'static web page where you cand find you favorite characters from Breaking Bad'}
+                    />
+                </Col>
+
+                <Col xs={10} sm={8} md={5} lg={5}>
+                    <Proyect
+                        title={'Breaking Bad Cast'}
+                        image={'https://github.com/franciscoG98/BreakingBadApp2/blob/main/src/img/home.png'}
+                        imgAlt={'breaking bad cast site preview'}
+                        repo={'https://github.com/franciscoG98/BreakingBadApp2'}
+                        // siteLink={}
+                        description={'static web page where you cand find you favorite characters from Breaking Bad'}
+                    />
+                </Col> */}
+            </Row>
 
             
-            {/* <div className="boxx">
-                <h4>work title</h4>
-                <img src="#" alt="imagen"/>
-                <div className="moreInfo">
-                    <span>repo</span>
-                    <br/>
-                    <span>deployed</span>
-                </div>
-            </div>
-            <div className="boxx">
-                <h4>work title</h4>
-                <img src="#" alt="imagen"/>
-                <div className="moreInfo">
-                    <span>repo</span>
-                    <br/>
-                    <span>deployed</span>
-                </div>
-            </div>
-            <div className="boxx">
-                <h4>work title</h4>
-                <img src="#" alt="imagen"/>
-                <div className="moreInfo">
-                    <span>repo</span>
-                    <br/>
-                    <span>deployed</span>
-                </div>
-            </div>
-            <div className="boxx">
-                <h4>work title</h4>
-                <img src="#" alt="imagen"/>
-                <div className="moreInfo">
-                    <span>repo</span>
-                    <br/>
-                    <span>deployed</span>
-                </div>
-            </div> */}
+          
         </div>
     )
 }
