d9586185d7b9c48eb7c786e3888bef272e57ea61
Francisco <franciscomanuel.gonzalez3@gmail.com>
2021-07-11 19:15:24 -0300

    added portfolio to works, change image size of proyect component

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -10,17 +10,42 @@ const Work = () => {
     const BBClink = 'https://franciscog98.github.io/BreakingBadApp2/';
     const BBCrepo = 'https://github.com/franciscoG98/BreakingBadApp2';
 
-    const Moviesimg = 'https://raw.githubusercontent.com/franciscoG98/movies-app/main/media/homeScreen.png';
-    const Movieslink = 'https://movies-app-fg.vercel.app/';
-    const Moviesrepo = 'https://github.com/franciscoG98/movies-app';
-    
-    
+    const MoviesImg = 'https://raw.githubusercontent.com/franciscoG98/movies-app/main/media/homeScreen.png';
+    const MoviesLink = 'https://movies-app-fg.vercel.app/';
+    const MoviesRepo = 'https://github.com/franciscoG98/movies-app';
+
+    const PortfolioImg = 'https://raw.githubusercontent.com/franciscoG98/portfolio/master/public/img/homePic.png';
+    const PortfolioLink = 'https://franciscog98.github.io/portfolio/';
+    const PortfolioRepo = 'https://github.com/franciscoG98/portfolio';
 
     return (
         
         <div className='workContainer'>
 
             <Row className='cardRow'>
+
+                <Col xs={10} sm={8} md={5} lg={5}>
+                    <Proyect
+                        title={'Personal Portfolio'}
+                        image={PortfolioImg}
+                        imgAlt={'portfolio site preview'}
+                        repo={PortfolioRepo}
+                        siteLink={PortfolioLink}
+                        description={'This site, where I show my skills, proyects and contact information. Stack: react, css, react, react-bootstrap, github pages'}
+                    />
+                </Col>
+
+                <Col xs={10} sm={8} md={5} lg={5}>
+                    <Proyect
+                        title={'Movies App'}
+                        image={MoviesImg}
+                        imgAlt={'movies app site preview'}
+                        repo={MoviesRepo}
+                        siteLink={MoviesLink}
+                        description={'Static web page where you cand find you favorite movies and series. Stack: react, css, OMDB Api, vercel'}
+                    />
+                </Col>
+
                 <Col xs={10} sm={8} md={5} lg={5}>
                     <Proyect
                         title={'Breaking Bad'}
@@ -32,38 +57,6 @@ const Work = () => {
                     />
                 </Col>
 
-                <Col xs={10} sm={8} md={5} lg={5}>
-                    <Proyect
-                        title={'Movies App'}
-                        image={Moviesimg}
-                        imgAlt={'movies app site preview'}
-                        repo={Moviesrepo}
-                        siteLink={Movieslink}
-                        description={'Static web page where you cand find you favorite movies and series. Stack: react, css, OMDB Api, vercel'}
-                    />
-                </Col>
-
-                {/* <Col xs={10} sm={8} md={5} lg={5}>
-                    <Proyect
-                        title={'Breaking Bad Cast'}
-                        image={'https://github.com/franciscoG98/BreakingBadApp2/blob/main/src/img/home.png'}
-                        imgAlt={'breaking bad cast site preview'}
-                        repo={'https://github.com/franciscoG98/BreakingBadApp2'}
-                        // siteLink={}
-                        description={'static web page where you cand find you favorite characters from Breaking Bad'}
-                    />
-                </Col>
-
-                <Col xs={10} sm={8} md={5} lg={5}>
-                    <Proyect
-                        title={'Breaking Bad Cast'}
-                        image={'https://github.com/franciscoG98/BreakingBadApp2/blob/main/src/img/home.png'}
-                        imgAlt={'breaking bad cast site preview'}
-                        repo={'https://github.com/franciscoG98/BreakingBadApp2'}
-                        // siteLink={}
-                        description={'static web page where you cand find you favorite characters from Breaking Bad'}
-                    />
-                </Col> */}
             </Row>
 
             
